bfe2a9468a1a51dfb8dda42cb3be21a9c74d4065
Krystian2004-cyber <krystian.kisielow@gmail.com>
2024-07-16 01:12:56 +0200

    saSas

--- reactAPI-SPA/src/elements/Body.tsx
+++ reactAPI-SPA/src/elements/Body.tsx
@@ -4,16 +4,24 @@ import Favourites from "./Favourites/Favourites";
 import Login from "./Login/Login";
 import Allitems from "./Allitems/Allitems";
 import CarDetails from "./CarDetails/CarDetails";
+import useGlobalState from "../context/useGlobalState";
+
 
 function Body () {
+  const { state} = useGlobalState();
 
     return (
         <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/allitems" element={<Allitems />} />
+        {state.user?.token && 
+        <>
+        <Route path="/favourites" element={<Favourites />} />
         <Route path="/cardetails" element={<CarDetails />} />
+        <Route path="/allitems" element={<Allitems />} />
+        </>
+}
+
 
       </Routes>
     )
--- reactAPI-SPA/src/elements/Login/Login.tsx
+++ reactAPI-SPA/src/elements/Login/Login.tsx
@@ -35,6 +35,7 @@ const Login = () => {
         {
             console.log('dziala');
             setResponse('invalid credentials')
+            return;
         }
      
 
